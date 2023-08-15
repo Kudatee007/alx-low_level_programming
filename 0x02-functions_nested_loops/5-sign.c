f9108f7fc0466774d79f7b9c35be85e29a239880
Kudatee007 <Timilehinkudaisi01@gmail.com>
2023-08-15 16:30:05 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (10);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(45);
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
+		_putchar(48);
 		return (-1);
 	}
 }
