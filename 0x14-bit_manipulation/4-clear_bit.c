c7e78fa577381ca8c09994971c8f37a1de22d8e9
Timilehin <timilehinkudaisi01@gmail.com>
2023-09-28 10:44:50 -0700

    update changes for 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,15 +11,15 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int maff;
+	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	maff = 1UL << index;
-	*n &= ~maff;
+	mask = 1UL << index;
+	*n &= ~mask;
 
 	return (-1);
 }
